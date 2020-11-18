82dcb360b81a1c6f3cb42e6f8eaf76401591c07a
Art Bachmann <bachmann.art@gmail.com>
2020-11-18 07:30:09 +0200

    added google console verify

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -28,6 +28,7 @@ const SEO = ({ title, description, image, article }) => {
     <Helmet title={seo.title} titleTemplate={titleTemplate}>
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
+      <meta name="google-site-verification" content="dGWvZ6ZCxhKj45R9O328ZUu9SleAunima6EmT3Z3rqg" />
 
       {seo.url && <meta property="og:url" content={seo.url} />}
 
