1d31a8965dd89e8ff85618f329e92559ff20c756
Art Bachmann <bachmann.art@gmail.com>
2020-11-20 15:42:00 +0200

    back with google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -85,43 +85,29 @@ module.exports = {
     },
 
     {
-      resolve: 'gatsby-plugin-webfonts',
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
-        fonts: {
-          google: [
-            {
-              family: "Roboto",
-              variants: ["300", "400", "500", "600"],
-            },
-            {
-              family: "Merriweather Sans",
-              variants: ['400', '500', '600', '700'],
-              text: "Hello",
-              formatAgents: {
-                woff: `Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; rv:11.0) like Gecko`,
-                woff2: `Mozilla/5.0 (Windows NT 10.0; Win64; x64; ServiceUI 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393`,
-              },
-            },
-            {
-              family: "Open Sans Condensed",
-              variants: ['300', '400', '500', '600', '700'],
-              text: "Hello",
-              formatAgents: {
-                woff: `Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; rv:11.0) like Gecko`,
-                woff2: `Mozilla/5.0 (Windows NT 10.0; Win64; x64; ServiceUI 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393`,
-              },
-            },
-            {
-              family: "Montserrat",
-              variants: [`400`, `500`, `600`, `700`],
-              text: "Hello",
-              formatAgents: {
-                woff: `Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; rv:11.0) like Gecko`,
-                woff2: `Mozilla/5.0 (Windows NT 10.0; Win64; x64; ServiceUI 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393`,
-              },
-            },
-          ],
-        }
+        fonts: [
+          {
+            family: "Roboto",
+            variants: ["300", "400", "500", "600"],
+          },
+          {
+            family: "Merriweather Sans",
+            variants: ['400', '500', '600', '700'],
+            text: "Hello",
+          },
+          {
+            family: "Open Sans Condensed",
+            variants: ['300', '400', '500', '600', '700'],
+            text: "Hello",
+          },
+          {
+            family: "Montserrat",
+            variants: [`400`, `500`, `600`, `700`],
+            text: "Hello",
+          },
+        ],
       },
     },
   ],
